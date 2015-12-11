5182dea8b4eef66f0dd9a7f9447dca0f68338b5e
Jianqing Peng <pengjianqing@gmail.com>
2015-12-11 10:04:31 +0800

    add city

---
+++ ChinaPM25/CityCell.h
@@ -0,0 +1,13 @@
+//
+//  CityCell.h
+//  ChinaPM25
+//
+//  Created by Jianqing Peng on 12/11/15.
+//  Copyright © 2015 Jianqing Peng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CityCell : UITableViewCell
+
+@end
---
+++ ChinaPM25/CityCell.m
@@ -0,0 +1,23 @@
+//
+//  CityCell.m
+//  ChinaPM25
+//
+//  Created by Jianqing Peng on 12/11/15.
+//  Copyright © 2015 Jianqing Peng. All rights reserved.
+//
+
+#import "CityCell.h"
+
+@implementation CityCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
